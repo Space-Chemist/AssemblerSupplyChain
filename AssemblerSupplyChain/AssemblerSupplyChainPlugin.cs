e6767f8c711d5acb0e0ad97237d811191245e2ea
christopher leverenz <cleverenz21@gmail.com>
2021-02-09 17:35:49 -0800

    Initial commit

---
+++ AssemblerSupplyChain/AssemblerSupplyChainPlugin.cs
@@ -0,0 +1,16 @@
+﻿using Torch;
+using Torch.API;
+
+namespace AssemblerSupplyChain
+{
+    public class AssemblerSupplyChainPlugin
+    {
+        public class NoIdlePlugin : TorchPluginBase
+        {
+            public override void Init(ITorchBase torch)
+            {
+                base.Init(torch);
+            }
+        }
+    }
+}
---
+++ AssemblerSupplyChain/Patch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Reflection;
+using NLog;
+using Sandbox.Game.Entities.Cube;
+using Sandbox.Game.Entities.Inventory;
+using Sandbox.Game.Weapons;
+using Torch.Managers.PatchManager;
+using VRage.Game.Entity;
+using VRage.Game.ModAPI;
+
+namespace AssemblerSupplyChain
+{
+    public class Patch
+    {
+        [PatchShim]
+        public static class MyAssemblerBasePatch {
+
+            public static readonly Logger Log = LogManager.GetCurrentClassLogger();
+
+            internal static readonly MethodInfo UpdateMethod =
+                typeof(MyAssembler).GetMethod(nameof(MyAssembler.UpdateBeforeSimulation100), BindingFlags.Instance | BindingFlags.Public) ??
+                throw new Exception("Failed to find patch method");
+
+            internal static readonly MethodInfo IdleSupplyPatch =
+                typeof(MyAssemblerBasePatch).GetMethod(nameof(IdleSupply), BindingFlags.Static | BindingFlags.Public) ??
+                throw new Exception("Failed to find patch method");
+
+            public static void Patch(PatchContext ctx) {
+
+                ctx.GetPattern(UpdateMethod).Prefixes.Add(IdleSupplyPatch);
+
+                Log.Debug("Patching Successful MyLargeTurretBase!");
+            }
+
+            public static void IdleSupply(MyAssembler __instance) {
+          
+                if (!(__instance is MyAssembler assembler))
+                    return;
+
+                if (assembler.IsQueueEmpty)
+                {
+                    var amount = assembler.InventoryCount;
+                    remove => this.CurrentStateChanged -= this.GetDelegate(value);
+                }
+
+                return;
+            }
+        }
+    }
+}
