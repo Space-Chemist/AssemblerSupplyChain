65babacd02215f65ab891069a1744c2b6bb2bd71
SpaceChemist <cleverenz21@gmail.com>
2021-02-10 03:05:47 -0800

    ran in circles am dumb

--- AssemblerSupplyChain/AssemblerSupplyChainPlugin.cs
+++ AssemblerSupplyChain/AssemblerSupplyChainPlugin.cs
@@ -1,16 +1,24 @@
-﻿using Torch;
+﻿using System.Runtime.InteropServices;
+using Sandbox.Game;
+using Sandbox.Game.Entities;
+using Sandbox.Game.Entities.Cube;
+using Torch;
 using Torch.API;
+using Torch.Session;
+using VRage.Network;
+using VRage.Sync;
 
 namespace AssemblerSupplyChain
 {
-    public class AssemblerSupplyChainPlugin
+    public class AssemblerSupplyChainPlugin : TorchPluginBase 
     {
-        public class NoIdlePlugin : TorchPluginBase
+        public MyInventoryConstraint _inventoryConstraint = new MyInventoryConstraint(string.Empty);
+        public static AssemblerSupplyChainPlugin Instance { get; private set; }
+        public readonly int MAX_ITEMS_TO_PULL_IN_ONE_TICK = 10;
+        public static TorchSessionManager SessionManager;
+        public override void Init(ITorchBase torch)
         {
-            public override void Init(ITorchBase torch)
-            {
-                base.Init(torch);
-            }
+            base.Init(torch);
         }
     }
 }
--- AssemblerSupplyChain/Patch.cs
+++ AssemblerSupplyChain/Patch.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 using NLog;
+using Sandbox.Game;
+using Sandbox.Game.Entities;
 using Sandbox.Game.Entities.Cube;
 using Sandbox.Game.Entities.Inventory;
+using Sandbox.Game.GameSystems;
+using Sandbox.Game.GameSystems.Conveyors;
 using Sandbox.Game.Weapons;
 using Torch.Managers.PatchManager;
+using VRage;
+using VRage.Game;
 using VRage.Game.Entity;
 using VRage.Game.ModAPI;
+using VRage.Sync;
+using IMyInventoryItem = VRage.Game.ModAPI.Ingame.IMyInventoryItem;
 
 namespace AssemblerSupplyChain
 {
@@ -37,10 +46,54 @@ namespace AssemblerSupplyChain
                 if (!(__instance is MyAssembler assembler))
                     return;
 
-                if (assembler.IsQueueEmpty)
+                if (assembler.IsQueueEmpty && assembler.HasInventory)
                 {
-                    var amount = assembler.InventoryCount;
-                    remove => this.CurrentStateChanged -= this.GetDelegate(value);
+                    //figure out how to see if assembler is even connected to conveyors cause fuck me 
+                    if (//assembler.fuckme)
+                    {
+                        try
+                        {
+                            
+                            var group =assembler.CubeGrid.BigOwners;
+                            var me =group.First();
+                            MyInventory inventory = assembler.GetInventory();
+                            if ((double) inventory.VolumeFillFactor >= 0.990000009536743)
+                                return;
+                            MyGridConveyorSystem.PullAllItemsForConnector((IMyConveyorEndpointBlock) assembler, inventory, me, AssemblerSupplyChainPlugin.Instance.MAX_ITEMS_TO_PULL_IN_ONE_TICK);
+
+                        }
+                        catch (Exception e)
+                        {
+                            Log.Fatal("fuck my whole life" + e);
+                        }
+                    }    
+                    
+                    
+                    /*MyInventory inventory = assembler.GetInventory();
+                    if (inventory.GetItemsCount() <= 0)
+                        return;
+                    var blocks = assembler.CubeGrid.GridSystems.ConveyorSystem.
+                    assembler.CubeGrid.GridSystems.ConveyorSystem.PullItems(_inventoryConstraint, MyFixedPoint.MaxValue, blocks, inventory );*/
+
+                    
+
+                    // this probably just yeets the contents of the assembler
+                    //assembler.Components.Remove<MyInventory>();
+
+
+                    /*MyGridConveyorSystem.PushAnyRequest((inventory) this, inventory);
+                    if (assembler.GetInventory() == null)
+                    {
+                        MyInventory myInventory = new MyInventory(assembler.InventorySize.Volume, this.m_conveyorSorterDefinition.InventorySize, MyInventoryFlags.CanSend);
+                        assembler.Components.Add<MyInventoryBase>((MyInventoryBase) myInventory);
+                        myInventory.Init(builderConveyorSorter.Inventory);
+                    }
+                    var a = assembler.OutputInventory.InventoryId;
+                    var i = assembler.ConveyorEndpoint.CubeBlock.InventoryCount;
+                    assembler.Components.Clear();
+                    assembler.drainall();
+                    MyConveyorSorter sorter;
+                    sorter.DrainAll = true;*/
                 }
 
                 return;
